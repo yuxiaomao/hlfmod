6780c2931ea180582813e8bc59a96241cb73eafd
Yuxiao Mao <yuxiao.mao@outlook.com>
2024-09-24 10:36:02 +0200

    Add log config (L dll), add some Api functions

--- src/fmod/Api.hx
+++ src/fmod/Api.hx
@@ -4,27 +4,42 @@ class Api {
 	static var initialized = false;
 	static var system : Native.System;
 	static var basePath : String;
+	static var loadedBank : Map<String, Native.Bank>;
 
-	public static function init(basePath : String, masterBank : String) {
+	public static function init(path : String, masterBank : String) {
 		system = Native.System.create();
 		// set additional config here
 		system.initialize(32, NORMAL, NORMAL, null);
+		basePath = path + "/";
+		loadedBank = [];
 		initialized = true;
-		Api.basePath = basePath + "/";
-		system.loadBankFile(@:privateAccess (basePath + masterBank).toUtf8(), NORMAL);
+		loadedBank(masterBank);
 	}
 
 	public static function release() {
+		if (!initialized) return;
 		system.release();
+		initialized = false;
+	}
+
+	public static function update() {
+		if (!initialized) return;
+		system.update();
 	}
 
 	public static function loadBank(name : String) {
 		if (!initialized) return;
-		system.loadBankFile(name, NORMAL);
+		var p = basePath + name;
+		system.loadBankFile(@:privateAccess p.toUtf8(), NORMAL);
 	}
 
 	public static function unloadBank(name : String) {
-
+		if (!initialized) return;
+		var bank = loadedBank.get(name);
+		if (bank != null) {
+			bank.unload();
+			loadedBank.remove(name);
+		}
 	}
 
 }
